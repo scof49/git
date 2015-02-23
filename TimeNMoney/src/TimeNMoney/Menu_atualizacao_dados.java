02f2567876e18775abb040323e552c9c381d6aba
scof49 <ivojboliveira@gmail.com>
2015-02-23 16:04:48 +0000

    commit teste

--- TimeNMoney/src/TimeNMoney/Menu_atualizacao_dados.java
+++ TimeNMoney/src/TimeNMoney/Menu_atualizacao_dados.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class Menu_atualizacao_dados extends JFrame {
 
 	private JPanel contentPane;
+	private JLabel lblMenuAtualizarDados;
 
 	/**
 	 * Launch the application.
@@ -31,12 +35,20 @@ public class Menu_atualizacao_dados extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu_atualizacao_dados() {
+		initComponents();
+	}
+	private void initComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		lblMenuAtualizarDados = new JLabel("Menu Atualizar Dados");
+		lblMenuAtualizarDados.setIcon(new ImageIcon(Menu_atualizacao_dados.class.getResource("/TimeNMoney/receive_icon.png")));
+		lblMenuAtualizarDados.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblMenuAtualizarDados.setBounds(10, 11, 414, 30);
+		contentPane.add(lblMenuAtualizarDados);
 	}
-
 }
