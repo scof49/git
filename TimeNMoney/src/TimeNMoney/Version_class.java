78a281004da1564fbce3cc6c27911062cee2df48
scof49 <ivojboliveira@gmail.com>
2015-02-27 11:50:34 +0000

    version

--- TimeNMoney/src/TimeNMoney/Version_class.java
+++ TimeNMoney/src/TimeNMoney/Version_class.java
@@ -4,7 +4,7 @@ public class Version_class {
 	public String version;
 	
 	public Version_class(){
-		this.version  = "1.0.4.0";
+		this.version  = "1.0.5.0";
 	}
 	
 	public String get_version(){return this.version;}
